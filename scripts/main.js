5f6f40369e30425c2ae7bfa9c15c803b3d0ba288
subhash kumar singh <subh.singh007@gmail.com>
2018-03-02 23:15:51 +0530

    add image on click action

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,20 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+// var myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
 
-document.querySelector('html').onclick = function() {
-  alert("please don't distrub")
+// document.querySelector('html').onclick = function() {
+//   alert("please don't distrub")
+// }
+
+var myImage = document.querySelector('img');
+var myText = document.querySelector('h1');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/twitter-logo.png');
+      myText.textContent = "twitter is microblogging"
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+      myText.textContent = "firefox is browser"
+    }
 }
