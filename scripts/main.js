abd2e8c7bf84578a71fb5f383f9abbffa6d907d7
subhash kumar singh <subh.singh007@gmail.com>
2018-03-03 16:24:03 +0530

    example for local storage

--- scripts/main.js
+++ scripts/main.js
@@ -8,13 +8,31 @@
 var myImage = document.querySelector('img');
 var myText = document.querySelector('h1');
 
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/twitter-logo.png');
-      myText.textContent = "twitter is microblogging"
-    } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
-      myText.textContent = "firefox is browser"
-    }
+var myButton = document.querySelector('button');
+myButton.onclick = function() {
+  setUsername();
+}
+
+// myImage.onclick = function() {
+//     var mySrc = myImage.getAttribute('src');
+//     if(mySrc === 'images/firefox-icon.png') {
+//       myImage.setAttribute ('src','images/twitter-logo.png');
+//       myText.textContent = "twitter is microblogging"
+//     } else {
+//       myImage.setAttribute ('src','images/firefox-icon.png');
+//       myText.textContent = "firefox is browser"
+//     }
+// }
+
+if (localStorage.getItem('name')) {
+  var storedName = localStorage.getItem('name');
+  myText.textContent = 'wecome back ' + storedName;
+} else {
+  setUsername();
+}
+
+function setUsername() {
+  var myName = prompt('please enter your name');
+  localStorage.setItem('name', myName);
+  myText.textContent = "welcome to 0 to hero " + myName;
 }
